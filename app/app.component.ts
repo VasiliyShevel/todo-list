6fe133991b2c4e2497ad4d9af83a611da4e8f1f5
VasiliyShevel <vasek904@yandex.ru>
2017-03-05 15:53:26 +0200

    Another commnet

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,6 +9,9 @@
 /* This is a comment */
 /* This is a comment */
 /* This is a comment */
+/* This is a comment */
+/* This is a comment */
+/* This is a comment */
 
 
 import { Component } from '@angular/core';
